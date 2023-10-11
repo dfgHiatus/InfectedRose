5d9a8e21a38fa8d21429b5c24243340a5ce114ea
frozenreflex <frozenreflex1@gmail.com>
2023-10-10 23:12:09 -0500

    Terrain and hiding renderers

--- InfectedRose.Resonite/LegoUniverseImporter.cs
+++ InfectedRose.Resonite/LegoUniverseImporter.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using InfectedRose.Terrain;
 using AlphaHandling = FrooxEngine.AlphaHandling;
 
 namespace InfectedRose.Resonite;
@@ -116,7 +117,35 @@ public class LegoUniverseImporter : ResoniteMod
         await default(ToBackground);
 
         Msg($"Parsing luz file");
-        var lvls = new List<LvlFile>();
+        
+        {
+            Msg($"Parsing terrain file");
+            using var stream = new FileStream(Path.Combine(Path.GetDirectoryName(path), file.TerrainFileName), FileMode.Open, FileAccess.Read);
+            using var reader = new BitReader(stream);
+            var terrain = new TerrainFile();
+            terrain.Deserialize(reader);
+            
+            await default(ToWorld);
+            var terrainSlot = root.AddSlot("Terrain");
+            await default(ToBackground);
+            
+            var mesh = terrain.ToFrooxEngine();
+
+            var localDb = Engine.Current.LocalDB;
+            var tempFilePath = localDb.GetTempFilePath(".meshx");
+            mesh.SaveToFile(tempFilePath);
+            var url = localDb.ImportLocalAssetAsync(tempFilePath, LocalDB.ImportLocation.Move).Result;
+
+            await default(ToWorld);
+            var staticMesh = terrainSlot.AttachComponent<StaticMesh>();
+            staticMesh.URL.Value = url;
+            
+            var meshRenderer = terrainSlot.AttachComponent<MeshRenderer>();
+            meshRenderer.Mesh.Target = staticMesh;
+            await default(ToBackground);
+        }
+        
+        var lvls = new Dictionary<string, LvlFile>();
         var clientDirectory = config.GetValue(cdclientDirectory);
         var clientBaseDirectory = Path.GetDirectoryName(clientDirectory);
         
@@ -129,15 +158,10 @@ public class LegoUniverseImporter : ResoniteMod
             using var reader = new BitReader(stream);
             var lvl = new LvlFile();
             lvl.Deserialize(reader);
-            lvls.Add(lvl);
+            lvls.Add(p, lvl);
         }
         Msg($"Got {lvls.Count} lvl files");
-        var ids = lvls.
-            Where(i => i.LevelObjects is not null).
-            SelectMany(i => i.LevelObjects.Templates).
-            Select(i => i.Lot).
-            Distinct().
-            ToList();
+        var ids = lvls.Select(i => i.Value).Where(i => i.LevelObjects is not null).SelectMany(i => i.LevelObjects.Templates).Select(i => i.Lot).Distinct().ToList();
         Msg($"Got {ids.Count} unique lots");
         
         using var clientStream = new FileStream(clientDirectory, FileMode.Open, FileAccess.Read);
@@ -214,15 +238,38 @@ public class LegoUniverseImporter : ResoniteMod
         var sBlock = root.AddSlot("Scene");
         sBlock.LocalScale = float3.One * 0.25f;
 
-        foreach (var objects in lvls.Where(i => i.LevelObjects is not null).SelectMany(i => i.LevelObjects.Templates))
+        foreach (var lvl in lvls.Where(i => i.Value.LevelObjects is not null))
         {
-            var template = objectTemplates.FindChild(objects.Lot.ToString());
-            if (template is not null)
+            foreach (var objects in lvl.Value.LevelObjects.Templates)
             {
-                var o = template.Duplicate(sBlock, false);
-                o.LocalPosition = objects.Position.ToFrooxEngine();
-                o.LocalRotation = objects.Rotation; //TODO
-                o.LocalScale = new float3(objects.Scale, objects.Scale, objects.Scale);
+                var template = objectTemplates.FindChild(objects.Lot.ToString());
+                if (template is not null)
+                {
+                    var o = template.Duplicate(sBlock, false);
+                    o.LocalPosition = objects.Position.ToFrooxEngine();
+                    o.LocalRotation = objects.Rotation; //TODO
+                    o.LocalScale = new float3(objects.Scale, objects.Scale, objects.Scale);
+                    if (objects.LegoInfo.TryGetValue("renderDisabled", out var disabled) && (bool) disabled)
+                    {
+                        foreach (var mesh in o.GetComponentsInChildren<MeshRenderer>())
+                        {
+                            mesh.Enabled = false;
+                        }
+                    }
+                    else
+                    {
+                        var scene = file.Scenes.FirstOrDefault(i => i.FileName == lvl.Key);
+                        if (objects.LegoInfo.TryGetValue("sceneIDOverrideEnabled", out var shouldOverride) &&
+                            (bool) shouldOverride && objects.LegoInfo.TryGetValue("sceneIDOverride", out var over) &&
+                            (int) over != scene?.SceneId)
+                        {
+                            foreach (var mesh in o.GetComponentsInChildren<MeshRenderer>())
+                            {
+                                mesh.Enabled = false;
+                            }
+                        }
+                    }
+                }
             }
         }
         await default(ToBackground);
