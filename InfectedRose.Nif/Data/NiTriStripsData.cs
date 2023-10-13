4be7429d314a4dbd81c53551a30e836f979b2e69
frozenreflex <frozenreflex1@gmail.com>
2023-10-13 16:07:04 -0500

    Fix tristripsdata

--- InfectedRose.Nif/Data/NiTriStripsData.cs
+++ InfectedRose.Nif/Data/NiTriStripsData.cs
@@ -7,7 +7,7 @@ public class NiTriStripsData : NiTriBasedGeomData
 {
     public ushort StripsCount { get; set; }
 
-    public ushort StripsLengths { get; set; }
+    public ushort[] StripsLengths { get; set; }
 
     public bool HasPoints { get; set; }
 
@@ -17,9 +17,8 @@ public class NiTriStripsData : NiTriBasedGeomData
     {
         base.Deserialize(reader);
         StripsCount = reader.Read<ushort>();
-        StripsLengths = reader.Read<ushort>();
-        HasPoints = reader.Read<byte>() != 0;
-        if (!HasPoints) return;
-        Points = reader.Read2DArray<ushort>(StripsCount, StripsLengths);
+        StripsLengths = reader.ReadArray<ushort>(StripsCount);
+        HasPoints = reader.ReadBool();
+        if (HasPoints) Points = reader.Read2DArray<ushort>(StripsCount, StripsLengths);
     }
 }
